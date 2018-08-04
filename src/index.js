29c2a371d1abb1eaeedf568fc7aa22fe25f6bf3e
longtime1116 <longtime1116@gmail.com>
2018-08-05 08:31:40 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './components/Game';
+import './index.css';
+
+const mountElement = document.getElementById('root');
+
+ReactDOM.render(<Game />, mountElement);
