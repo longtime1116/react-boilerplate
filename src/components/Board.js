9a054712c15c867eb8bf5020867370ea84ce8d33
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-05 09:13:37 +0900

    Taking Turns

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,8 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
@@ -20,8 +21,11 @@ export default class Board extends React.Component {
 
   handleClick = i => {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   };
 
   // ↓だとエラーになる。onClick()に handleClick 関数そのものを渡してしまうので、Square コンポーネント
@@ -37,7 +41,7 @@ export default class Board extends React.Component {
   //}
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
