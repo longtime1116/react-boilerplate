7478dd426aa8ff7b4721599225c7c12e06194d59
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-05 09:43:20 +0900

    Lifting State Up, Again(not add history)

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,60 +2,18 @@ import React from "react";
 import Square from "./Square";
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
-  }
-
   renderSquare(i) {
     return (
       <Square
-        onClick={() => this.handleClick(i)}
-        value={this.state.squares[i]}
+        onClick={() => this.props.onClick(i)}
+        value={this.props.squares[i]}
       />
     );
   }
 
-  handleClick = i => {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  };
-
-  // ↓だとエラーになる。onClick()に handleClick 関数そのものを渡してしまうので、Square コンポーネント
-  // の方でクリックされたときに this がなにかわからなくなってしまう。
-  //renderSquare(i) {
-  //  return <Square onClick={this.handleClick} value={this.state.squares[i]} />;
-  //}
-
-  //handleClick() {
-  //  const squares = this.state.squares.slice();
-  //  squares[0] = "X";
-  //  this.setState({ squares: squares });
-  //}
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -74,25 +32,4 @@ export default class Board extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
